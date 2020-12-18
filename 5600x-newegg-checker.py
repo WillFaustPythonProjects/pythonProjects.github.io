fbf6853452f55c778b6988198b14051df9ff11a3
willfaust <39139570+willfaust@users.noreply.github.com>
2020-12-18 20:06:17 +0000

    update 5600x checker script to send notifications and some other things

--- 5600x-newegg-checker.py
+++ 5600x-newegg-checker.py
@@ -1,9 +1,38 @@
-import requests
 import lxml.html
+from urllib.request import urlopen
+import time
+from notify_run import Notify
+from win10toast import ToastNotifier
+
+n = 1
+
 while True:
-	html = requests.get('https://newegg.com/amd-ryzen-5-5600x/p/N82E16819113666')
-	site = lxml.html.fromstring(html.content)
-	try:
-		print(site.xpath('//*[@class="nav-col"]/span/text()')[0])
-	except IndexError:
-		print("In stock")
+  try:
+    html = urlopen('https://www.newegg.com/amd-ryzen-5-5600x/p/N82E16819113666').read().decode("utf-8")
+    site = lxml.html.fromstring(html)
+  except lxml.etree.ParserError:
+    time.sleep(1)
+    continue
+
+  try:
+    status = site.xpath('//*[@class="nav-col"]/descendant::node()/text()')[0]
+  except IndexError:
+    print(str(n) + ". IndexError, retrying")
+    n = n + 1
+    time.sleep(2)
+    continue
+
+  if (status == "Sold Out"):
+    print(str(n) + ". Out of stock")
+    n = n + 1
+    continue
+  elif (status == "Add to cart "):
+    print(str(n) + ". In stock")
+    ToastNotifier().show_toast("5600x Newegg Checker:","The 5600x is in stock!",icon_path="ryzen.ico")
+    Notify().send('The 5600x is in stock!')
+    n = n + 1
+    continue
+  else:
+    print(str(n) + status)
+    n = n + 1
+    time.sleep(2)
